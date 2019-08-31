9247c2ad57c53f016d9ad2c44b92e8cdfc64a812
WataruTakahagi <watarut@outlook.com>
2019-08-31 14:49:20 +0900

    Update

--- enl_map.py
+++ enl_map.py
@@ -4,23 +4,22 @@ import pandas as pd
 
 class locality:
     def __init__(self):
-        self.locality = pd.DataFrame({'Nation':[], 'Location':[], 'Latitude':[], 'Longitude':[], 'Age(GA)':[],'Reference':[]})
+        self.locality = pd.DataFrame({'Nation':[], 'Location':[], 'Latitude':[], 'Longitude':[], 'Mineral':[], 'Age':[],'Reference':[]})
         self.zmap = folium.Map(location=[0, 0], zoom_start=3)
 
     def set_default(self,latitude,longitude,zoom):
         self.zmap = folium.Map(location=[latitude, longitude], zoom_start=zoom)
 
-    def collect(self,nation,location,latitude,longitude,age,reference):
-        add_locality = pd.DataFrame({'Nation':[nation], 'Location':[location], 'Latitude':[latitude], 'Longitude':[longitude], 'Age':[age],'Reference':[reference]},index=[len(self.locality)+1])
+    def collect(self,nation,location,latitude,longitude,mineral,age,reference):
+        add_locality = pd.DataFrame({'Nation':[nation], 'Location':[location], 'Latitude':[latitude], 'Longitude':[longitude], 'Mineral':[mineral], 'Age':[age],'Reference':[reference]},index=[len(self.locality)+1])
         self.locality  = pd.concat([self.locality,add_locality],sort=False)
 
     def add_map(self,num):
         ind = 'index == '+str(num)
-        latitude = self.locality.query(ind)['Latitude']
-        longitude = self.locality.query(ind)['Longitude']
-        pop = str(float(self.locality.query(ind)['Age']))+'(GA)'
+        latitude,longitude = self.locality.query(ind)['Latitude'],self.locality.query(ind)['Longitude']
+        pop = str(float(self.locality.query(ind)['Age']))+' (GA) '+str(self.locality.query(ind)['Mineral'])
         folium.Marker([latitude, longitude], popup=pop).add_to(self.zmap)
-        self.zmap.save('zmap.html')
+        self.zmap.save('mineral_age.html')
 
     def marker(self,num):
         if num == 'all':
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from enl_zircon import locality
+from enl_map import locality
 lc = locality()
 
-lc.collect('Australia','Pilbara',-21.8097056,117.3049005,4.2,'Etc et al., 2019')
-lc.collect('Australia','Pilbara',-23.8097056,119.3049005,4.2,'Etc et al., 2018')
-lc.collect('Australia','Pilbara',-24.0097056,119.3049005,4.3,'Etc et al., 2017')
+lc.collect('Australia','Pilbara',-21.8097056,117.3049005,'zircon',4.2,'Etc et al., 2019')
+lc.collect('Australia','Pilbara',-23.8097056,119.3049005,'zircon',4.2,'Etc et al., 2018')
+lc.collect('Australia','Pilbara',-24.0097056,119.3049005,'zircon',4.3,'Etc et al., 2017')
+
 lc.marker('all')
