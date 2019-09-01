b3ba6516abd2469d7340d545990a76ef9758c898
WataruTakahagi <watarut@outlook.com>
2019-09-01 11:48:20 +0900

    Update

--- enl_map.py
+++ enl_map.py
@@ -10,14 +10,22 @@ class locality:
     def set_default(self,latitude,longitude,zoom):
         self.zmap = folium.Map(location=[latitude, longitude], zoom_start=zoom)
 
-    def collect(self,nation,location,latitude,longitude,mineral,age,reference):
+    def manual_collect(self,nation,location,latitude,longitude,mineral,age,reference):
         add_locality = pd.DataFrame({'Nation':[nation], 'Location':[location], 'Latitude':[latitude], 'Longitude':[longitude], 'Mineral':[mineral], 'Age':[age],'Reference':[reference]},index=[len(self.locality)+1])
         self.locality  = pd.concat([self.locality,add_locality],sort=False)
+
+    def read_spreadsheet(self,url):
+        pass
+
+    def google_map(self,latitude,longitude):
+        url_bridge = 'https://www.google.com/maps/place/'+str(latitude)+'+'+str(longitude)
+        return url_bridge
 
     def add_map(self,num):
         ind = 'index == '+str(num)
         latitude,longitude = self.locality.query(ind)['Latitude'],self.locality.query(ind)['Longitude']
-        pop = str(float(self.locality.query(ind)['Age']))+' (GA) '#+str(self.locality.query(ind)['Mineral'])
+        link = self.google_map(float(latitude), float(longitude))
+        pop = str(float(self.locality.query(ind)['Age']))+' (GA)\n'+link
         folium.Marker([latitude, longitude], popup=pop).add_to(self.zmap)
         self.zmap.save('mineral_age.html')
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from enl_map import locality
 lc = locality()
 
-lc.collect('Australia','Pilbara',-21.8097056,117.3049005,'zircon',4.2,'Etc et al., 2019')
-lc.collect('Australia','Pilbara',-23.8097056,119.3049005,'zircon',4.2,'Etc et al., 2018')
-lc.collect('Australia','Pilbara',-24.0097056,118.3000005,'zircon',4.3,'Etc et al., 2017')
-lc.collect('Canada','Labrador',53.682000,-60.746118,'zircon',2.7,'Etc et al., 2016')
-lc.collect('Gabon','Okandja',-0.704010,13.791012,'zircon',2.4,'Etc et al., 2015')
+lc.set_default(0,0,3)
+lc.read_spreadsheet('https://docs.google.com/spreadsheets/d/1IUBUV8mGHp9lHNyCOh0FrJTupVpeLQA0W7sl6d7XiWM/edit#gid=1390728700')
+lc.manual_collect('Australia','Pilbara',-21.8097056,117.3049005,'zircon',4.2,'Etc et al., 2019')
+lc.manual_collect('Australia','Pilbara',-23.8097056,119.3049005,'zircon',4.2,'Etc et al., 2018')
+lc.manual_collect('Australia','Pilbara',-24.0097056,118.3000005,'zircon',4.3,'Etc et al., 2017')
+lc.manual_collect('Canada','Labrador',53.682000,-60.746118,'zircon',2.7,'Etc et al., 2016')
+lc.manual_collect('Gabon','Okandja',-0.704010,13.791012,'zircon',2.4,'Etc et al., 2015')
 
 lc.marker('all')
