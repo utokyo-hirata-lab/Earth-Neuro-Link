003f44a201e086e945451c2475802e15527e9e8f
WataruTakahagi <watarut@outlook.com>
2019-09-01 09:50:53 +0900

    Update

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ lc = locality()
 lc.collect('Australia','Pilbara',-21.8097056,117.3049005,'zircon',4.2,'Etc et al., 2019')
 lc.collect('Australia','Pilbara',-23.8097056,119.3049005,'zircon',4.2,'Etc et al., 2018')
 lc.collect('Australia','Pilbara',-24.0097056,118.3000005,'zircon',4.3,'Etc et al., 2017')
+lc.collect('Canada','Labrador',53.682000,-60.746118,'zircon',2.7,'Etc et al., 2016')
+lc.collect('Gabon','Okandja',-0.704010,13.791012,'zircon',2.4,'Etc et al., 2015')
 
 lc.marker('all')
